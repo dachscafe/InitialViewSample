3ef147a932d57edce76be9169174bb750422956f
YutoHonoki <0746034@mchcgr.com>
2023-02-03 20:27:43 +0900

    Initial Commit

---
+++ InitialViewSample/InitialViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InitialViewSampleApp.swift
+//  InitialViewSample
+//
+//  Created by Yuuto on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct InitialViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
